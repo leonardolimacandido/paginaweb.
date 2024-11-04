0015af0b02d226f4db32e3f87bfac38a13bd99ec
leonardolimacandido <165062457+leonardolimacandido@users.noreply.github.com>
2024-11-04 14:28:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que e programação ?',
+    'programação e o processo escrever e testar codigos para criar programas de computador'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da espanha?',
+    'A capital da epanha e madri'
+)
+
+criaCartao(
+    'Programação',
+    'O que é bug?',
+    'e o erro em um porograma'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'bom dia em ingles é good morning'
+)
